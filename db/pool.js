8cf32c7b78f52c8967db32d09d2e33a714767b53
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2026-01-02 11:20:23 +1100

    Change SSL for Koyeb deployment

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
+//Dev
+// const pool = new Pool({
+//   user: process.env.DEV_DB_USER,
+//   password: process.env.DEV_DB_PASSWORD,
+//   host: process.env.DEV_DB_HOST,
+//   port: process.env.DEV_DB_PORT,
+//   database: process.env.DEV_DB_NAME,
+// });
+
+//prod
+import postgres from 'postgres'
+
 const pool = new Pool({
-  user: process.env.DEV_DB_USER,
-  password: process.env.DEV_DB_PASSWORD,
-  host: process.env.DEV_DB_HOST,
-  port: process.env.DEV_DB_PORT,
-  database: process.env.DEV_DB_NAME,
-});
+  host: process.env.PROD_DB_HOST,
+  database: process.env.PROD_DB_NAME,
+  username: process.env.PROD_DB_USER,
+  password: process.env.PROD_DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false 
+  }
+})
 
 module.exports = pool;
